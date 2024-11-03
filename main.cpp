7ba4d9a5876cb6b64fe10c2c722effcc4b2e7110
Majkejl <misicek03@gmail.com>
2024-11-03 19:47:35 +0100

    added main

--- main.cpp
+++ main.cpp
@@ -7,5 +7,18 @@ int main (int, char**)
 	constexpr int height = 720;
 	Application app(width, height);
 
+#ifdef __EMSCRIPTEN__
+	// Equivalent of the main loop when using Emscripten:
+	auto callback = [](void *arg) {
+		Application* pApp = reinterpret_cast<Application*>(arg);
+		pApp->MainLoop(); // 4. We can use the application object
+	};
+	emscripten_set_main_loop_arg(callback, &app, 0, true);
+#else // __EMSCRIPTEN__
+	while (app.IsRunning()) {
+		app.MainLoop();
+	}
+#endif // __EMSCRIPTEN__
+
 	return 0;
 }
